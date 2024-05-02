550c88252b5a1d9288dcb935cecab35be5acf55a
Mikita <anyket2solo@gmail.com>
2024-05-02 21:56:24 +0300

    refactor: collect route names into constant enum

---
+++ pet-finder/src/constants/routes.ts
@@ -0,0 +1,10 @@
+/* eslint no-unused-vars: 0 */
+/* eslint no-shadow: 0 */
+export enum routes {
+  HOME = '/',
+  LOGIN = 'login',
+  REGISTER = 'register',
+  PROFILE = 'profile',
+  CONTENT = 'content',
+  NOT_FOUND = '*',
+}
