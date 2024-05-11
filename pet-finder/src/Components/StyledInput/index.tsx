9914b609ef3105098351d13a542dd3fd1fa5b9cb
Mikita <anyket2solo@gmail.com>
2024-05-11 23:35:49 +0300

    feat: create styled input component

---
+++ pet-finder/src/Components/StyledInput/index.tsx
@@ -0,0 +1,37 @@
+import { ReactElement, forwardRef } from 'react'
+import { FieldError } from 'react-hook-form'
+
+import style from './StyledInput.module.scss'
+
+interface StyledInputProps {
+  inputError?: FieldError
+  type: 'text' | 'password' | 'email'
+  inputName: 'name' | 'password' | 'email' | 'confirmPassword'
+  label: string
+}
+
+export const StyledInput = forwardRef<HTMLInputElement, StyledInputProps>(
+  (
+    { inputName, label, inputError, type, ...inputProps },
+    ref
+  ): ReactElement => (
+    <div className={style.inputContainer}>
+      <label className={style.styledLabel} htmlFor={inputName}>
+        {label}
+      </label>
+
+      <input
+        className={style.styledInput}
+        ref={ref}
+        type={type}
+        id={inputName}
+        key={inputName}
+        {...inputProps}
+      />
+
+      {inputError?.message && (
+        <span className={style.inputError}>{inputError?.message}</span>
+      )}
+    </div>
+  )
+)
