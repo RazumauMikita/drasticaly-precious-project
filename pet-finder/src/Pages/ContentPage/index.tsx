079ce4c77bb13f8ce1d21dbe9e3f35bdb7a2744a
Mikita <anyket2solo@gmail.com>
2024-06-16 00:02:50 +0300

    feat: add map component to content page

--- pet-finder/src/Pages/ContentPage/index.tsx
+++ pet-finder/src/Pages/ContentPage/index.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react'
 
+import { APIProvider } from '@vis.gl/react-google-maps'
 import style from './ContentPage.module.scss'
+import { MapComponent } from '../../Components/MapCpmponent'
 
 export const ContentPage: FC = () => (
   <section className={style.contentPage}>
-    <h1>ContentPage</h1>
+    <APIProvider
+      apiKey="AIzaSyAvrf86nTOVxt5xVWzTR0I1hiE6sXQj14w"
+      onLoad={() => console.log('Maps API has loaded.')}
+    >
+      <aside className={style.aside} />
+      <main className={style.main}>
+        <MapComponent />
+      </main>
+    </APIProvider>
   </section>
 )
