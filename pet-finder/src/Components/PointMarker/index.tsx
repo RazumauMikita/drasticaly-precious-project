c3d40475b07d8d1ed2973d8beae0604d1d33c808
Mikita <anyket2solo@gmail.com>
2024-06-16 17:03:18 +0300

    feat: setup infowindow component to markers onclick

--- pet-finder/src/Components/PointMarker/index.tsx
+++ pet-finder/src/Components/PointMarker/index.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
 import { FC, useCallback, useEffect, useRef, useState } from 'react'
-import { AdvancedMarker, Pin, useMap } from '@vis.gl/react-google-maps'
+import {
+  AdvancedMarker,
+  InfoWindow,
+  Pin,
+  useMap,
+} from '@vis.gl/react-google-maps'
 import { Marker, MarkerClusterer } from '@googlemaps/markerclusterer'
 
 import style from './PointMarker.module.scss'
@@ -19,12 +24,18 @@ export const PointMarker: FC<PointMarkerProps> = ({ points }) => {
   const map = useMap()
   const [markers, setMarkers] = useState<{ [key: string]: Marker }>({})
   const clusterer = useRef<MarkerClusterer | null>(null)
+  const [selectedPointKey, setSelectedPointKey] = useState<string | null>(null)
+  const [infoWindowShown, setInfoWindowShown] = useState(false)
+  const handleClose = useCallback(() => setInfoWindowShown(false), [])
+
   const handleClick = useCallback(
-    (ev: google.maps.MapMouseEvent) => {
+    (ev: google.maps.MapMouseEvent, key: string) => {
       if (!map) return
       if (!ev.latLng) return
       console.log('marker clicked: ', ev.latLng.toString())
       map.panTo(ev.latLng)
+      setSelectedPointKey(key)
+      setInfoWindowShown((isShown) => !isShown)
     },
     [map]
   )
@@ -57,21 +68,28 @@ export const PointMarker: FC<PointMarkerProps> = ({ points }) => {
 
   return (
     <div className={style.pointContainer}>
-      {points.map((poi: Point) => (
-        <AdvancedMarker
-          key={poi.key}
-          position={poi.location}
-          ref={(marker) => setMarkerRef(marker, poi.key)}
-          clickable
-          onClick={handleClick}
-        >
-          <Pin
-            background={poi.isLost ? `#FBBC04` : '#FF0000'}
-            glyphColor="#000"
-            borderColor="#000"
-          />
-        </AdvancedMarker>
-      ))}
+      {points &&
+        points.map((poi: Point) => (
+          <AdvancedMarker
+            key={poi.key}
+            position={poi.location}
+            ref={(marker) => setMarkerRef(marker, poi.key)}
+            clickable
+            onClick={(ev) => handleClick(ev, poi.key)}
+          >
+            <Pin
+              background={poi.isLost ? `#FBBC04` : '#FF0000'}
+              glyphColor="#000"
+              borderColor="#000"
+            />
+          </AdvancedMarker>
+        ))}
+      {infoWindowShown && selectedPointKey && (
+        <InfoWindow anchor={markers[selectedPointKey]} onClose={handleClose}>
+          <h2>InfoWindow content!</h2>
+          <p>Some arbitrary html to be rendered into the InfoWindow.</p>
+        </InfoWindow>
+      )}
     </div>
   )
 }
--- pet-finder/src/Pages/ContentPage/index.tsx
+++ pet-finder/src/Pages/ContentPage/index.tsx
@@ -1,32 +1,34 @@
 import { FC, useEffect, useState } from 'react'
-
 import { APIProvider } from '@vis.gl/react-google-maps'
-import style from './ContentPage.module.scss'
-import { MapComponent } from '../../Components/MapCpmponent'
-import { getAllLost } from '../../requests/req'
-import { ILost } from '../../requests/interfaces'
+
 import { Point } from '../../Components/PointMarker'
+import { MapComponent } from '../../Components/MapCpmponent'
+
+import { ILost } from '../../requests/interfaces'
+
+import { getAllLost } from '../../requests/req'
 import { getLocations } from '../../utils/getLocations'
+
+import style from './ContentPage.module.scss'
 
 export const ContentPage: FC = () => {
   const [locations, setLocations] = useState<Point[]>([])
+
   useEffect(() => {
     const fetchLost = async () => {
       try {
         const response: ILost[] = await getAllLost()
         setLocations(getLocations(response))
-      } catch (err) {
-        console.error(err)
+      } catch {
+        console.error()
       }
     }
     fetchLost()
   }, [])
+
   return (
     <section className={style.contentPage}>
-      <APIProvider
-        apiKey="AIzaSyAvrf86nTOVxt5xVWzTR0I1hiE6sXQj14w"
-        onLoad={() => console.log('Maps API has loaded.')}
-      >
+      <APIProvider apiKey="AIzaSyAvrf86nTOVxt5xVWzTR0I1hiE6sXQj14w">
         <aside className={style.aside} />
         <main className={style.main}>
           <MapComponent locations={locations} />
