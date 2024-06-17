2d0ada1387bed919b420c694b385868aeb16ace0
Andrei Niasmachny <Cejosoll@gmail.com>
2024-06-17 12:54:25 +0300

    feat: add error boundary

---
+++ pet-finder/src/Components/ErrorBoundary/index.tsx
@@ -0,0 +1,44 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+    const { hasError } = this.state
+    if (hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    const { children } = this.props;
+    return children;
+  }
+}
