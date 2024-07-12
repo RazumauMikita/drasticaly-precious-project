4b3e55de82a922daf667974bacc815fa4857b0f4
Andrei Niasmachny <Cejosoll@gmail.com>
2024-07-12 20:35:55 +0300

    refactor: correct misstyping and imports order

--- pet-finder/src/Components/LocationInputs/index.tsx
+++ pet-finder/src/Components/LocationInputs/index.tsx
@@ -1,14 +1,13 @@
 import { FC, useState } from "react";
-
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'; 
 
-import * as LocationInterfacres from '../../types/location.type';
+import * as LocationInterfaces from '../../types/location.type';
 
 const lang = 'en';
 
 
 export const LocationInput: FC = () => {
-const [location, setLocation] = useState<LocationInterfacres.Country | null>(null);
+const [location, setLocation] = useState<LocationInterfaces.Country | null>(null);
 console.log(location);
   return(
   <div>
